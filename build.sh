5510dfae0173000a90a63f80a31a774d4ceee1a4
brianhuster <phambinhanctb2004@gmail.com>
2024-05-06 20:39:24 +0700

    update header.html css

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+for arg in "$@"; do
+  if [ "$arg" = "-h" ] || [ "$arg" = "--help" ]; then
+    echo "Usage: ./build.sh [-s|--serve [port]]"
+    echo ""
+    echo "Options:"
+    echo "-s, --serve: After building the site, serve it using a Node.js HTTP server. If a port number is provided, the server will listen on that port. If no port number is provided, the server will listen on port 4000."
+    exit 0
+  fi
+done
+
 bundle exec jekyll build
 node assets/server/search_index.js
 
